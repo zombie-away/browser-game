76b7447715fc3279af1bd0aa6634c2a5f7a149d7
Pavel <pp-1995@mail.ru>
2017-04-25 22:54:47 +0500

    Рефаторинг

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,14 +1,22 @@
-var Player = require('../player.js');
-var Gun = require('../gun.js');
-var AK47 = require('../ak47.js');
-var Shotgun = require('../shotgun.js');
-var HealthBar = require('../HealthBar.js');
-var lifePanelConst = require('../constants/lifePanel.js');
-var textStyleKey = require('../constants/textStyle.js').textStyleKey;
-var textStyleValue = require('../constants/textStyle.js').textStyleValue;
+var Player = require('../player');
+var Gun = require('../gun');
+var AK47 = require('../ak47');
+var Shotgun = require('../shotgun');
+var HealthBar = require('../HealthBar');
+var lifePanelConst = require('../constants/lifePanel');
+var textStyleKey = require('../constants/textStyle').textStyleKey;
+var textStyleValue = require('../constants/textStyle').textStyleValue;
 
-function createPlayer() {
-
+function createPlayer(game, player) {
+    game.player = player;
+    game.game.add.existing(player);
+    game.game.camera.follow(player);
+    //weapon
+    var gun = new Gun(game.game, game.player);
+    var shotgun = new Shotgun(game.game, game.player);
+    game.player.weapon = gun;
+    game.player.backpack.weapons.push(gun);
+    game.player.backpack.weapons.push(shotgun);
 }
 
 function createMap() {
@@ -60,101 +68,107 @@ var game = {
     money: 0
 };
 
-game.create = function () {
-    // musicPlay(game);
-    game.worldMap = new WorldLoader(this.game, 'map');
-    // var cursor = game.add.sprite(0, 0, 'cursor');
-    // cursor.anchor.setTo(0.5, 0.5);
-    // game.physics.enable(cursor, Phaser.Physics.ARCADE);
-    game.input.addMoveCallback( function(pointer, x, y) {
-        // console.log(cursor);
-        // cursor.worldPosition = pointer.worldPosition;
-    });
-    //player
-    // game.player = new Player(this.game, game.world.centerX, game.world.height);
-    game.player = game.worldMap.player;
-    this.game.add.existing(game.player);
-    //weapon
-    var gun = new Gun(this.game, game.player);
-    var shotgun = new Shotgun(this.game, game.player);
-    game.player.weapon = gun;
-    game.player.backpack.weapons.push(gun);
-    game.player.backpack.weapons.push(shotgun);
-
-    this.game.camera.follow(game.player);
-
-    // game.lifePanelGroup.create(10, 10, 'lifePanel');
-    var panelCoordinates = {x: lifePanelConst.panelX, y: lifePanelConst.panelY};
-    addSprite(game, panelCoordinates, 'lifePanel');
-
-    var heartCoordinates = {x: lifePanelConst.heartX, y: lifePanelConst.heartY};
-    addSprite(game, heartCoordinates, 'heartForLifePanel');
-
-    var gunImagesCoordinates = {
-        x: lifePanelConst.gunForLifePanelX,
-        y: lifePanelConst.gunForLifePanelY
-    };
-    addSprite(game, gunImagesCoordinates, 'gunForLifePanel');
-
-
-    game.healthBar = new HealthBar(this.game, {x: lifePanelConst.healthBarX, y: lifePanelConst.healthBarY});
+function createHealthBar(game) {
+    game.healthBar = new HealthBar(game.game, {x: lifePanelConst.healthBarX, y: lifePanelConst.healthBarY});
     game.healthBar.setFixedToCamera(true);
     game.player.onHealthChange.add(function (percent) {
         game.healthBar.setPercent(percent);
     });
+}
 
+function createWeaponPanel(game) {
+    var bulletsTextCoords = {
+        x: lifePanelConst.bulletsTextX,
+        y: lifePanelConst.bulletsTextY
+    };
+    var text = `/${game.player.backpack.bullets[game.player.weapon.name].toString()}`;
+    game.bulletsTextValue = addText(game, bulletsTextCoords, text);
 
-    game.bulletsTextValue = game.add.text(
-        lifePanelConst.bulletsTextX,
-        lifePanelConst.bulletsTextY,
-        '/' + game.player.backpack.bullets[game.player.weapon.name].toString(),
-        textStyleValue
-    );
-    game.bulletsTextValue.fixedToCamera = true;
+    bulletsTextCoords = {
+        x: lifePanelConst.bulletsInGunTextX,
+        y: lifePanelConst.bulletsInGunTextY
+    };
+    text = game.player.weapon.bulletsInGun.toString();
+    game.bulletsInGunTextValue = addText(game, bulletsTextCoords, text);
+}
 
-    game.bulletsInGunTextValue = game.add.text(
-        lifePanelConst.bulletsInGunTextX,
-        lifePanelConst.bulletsInGunTextY,
-        game.player.weapon.bulletsInGun.toString(),
-        textStyleValue
-    );
-    game.bulletsInGunTextValue.fixedToCamera = true;
+function addText(game, coords, text) {
+    var textKey = game.add.text(coords.x, coords.y, text, textStyleKey);
+    textKey.fixedToCamera = true;
 
-    var scoreKey = game.add.text(32, 40, "SCORE",  textStyleKey);
-    scoreKey.fixedToCamera = true;
+    return textKey;
+}
 
-    game.scoreTextValue = game.add.text(130, 40, game.score.toString(),  textStyleValue);
-    game.scoreTextValue.fixedToCamera = true;
+function createTextPanel(game) {
+    addText(game, {x: 32, y: 40}, "SCOPE");
+    game.scoreTextValue = addText(game, {x: 130, y: 40}, game.score.toString());
+    addText(game, {x: 32, y: 70}, "MONEY");
+    game.moneyTextValue = addText(game, {x: 130, y: 70}, game.money.toString());
+}
 
+function createHeroPanel(game) {
+    var panelCoordinates = {x: lifePanelConst.panelX, y: lifePanelConst.panelY};
+    var heartCoordinates = {x: lifePanelConst.heartX, y: lifePanelConst.heartY};
+    var gunImagesCoordinates = {
+        x: lifePanelConst.gunForLifePanelX,
+        y: lifePanelConst.gunForLifePanelY
+    };
+    addSprite(game, panelCoordinates, 'lifePanel');
+    addSprite(game, heartCoordinates, 'heartForLifePanel');
+    addSprite(game, gunImagesCoordinates, 'gunForLifePanel');
+    createHealthBar(game);
+    createWeaponPanel(game);
+}
 
-    var moneyKey = game.add.text(32, 70, "MONEY",  textStyleKey);
-    moneyKey.fixedToCamera = true;
-    game.moneyTextValue = game.add.text(130, 70, game.money.toString(),  textStyleValue);
-    game.moneyTextValue.fixedToCamera = true;
+function createInterface(game) {
+    createHeroPanel(game);
+    createTextPanel(game);
+}
+
+game.create = function () {
+    game.worldMap = new WorldLoader(this.game, 'map');
+    //player
+    createPlayer(game, game.worldMap.player);
+    createInterface(game);
 };
 
 game.update = function () {
-    this.player.weapon.update();
+    game.player.weapon.update();
     //overlap noise zone and zombie
     game.physics.arcade.overlap(
-        this.player.noiseZone,
-        this.worldMap.zombies,
-        this.noiseZoneAndZombieCollision,
+        game.player.noiseZone,
+        game.worldMap.zombies,
+        game.noiseZoneAndZombieCollision,
         null,
-        this
+        game
     );
-    game.physics.arcade.collide(this.player, this.worldMap.collisionLayer);
-    game.physics.arcade.collide(this.worldMap.zombies, this.worldMap.collisionLayer);
-    game.physics.arcade.collide(this.player.weapon.bullets, this.worldMap.collisionLayer, this.bulletCollision, null, this);
+    game.physics.arcade.collide(game.player, game.worldMap.collisionLayer);
+    game.physics.arcade.collide(game.worldMap.zombies, game.worldMap.collisionLayer);
+    game.physics.arcade.collide(
+        game.player.weapon.bullets,
+        game.worldMap.collisionLayer,
+        game.bulletCollision,
+        null,
+        game
+    );
     game.physics.arcade.overlap(
-        this.player.weapon.bullets,
-        this.worldMap.zombies,
-        this.bulletAndZombieCollision,
-        null,
-        this
+        game.player.weapon.bullets,
+        game.worldMap.zombies,
+        game.bulletAndZombieCollision,
+        null, game
     );
-    game.physics.arcade.collide(this.player, this.worldMap.coins, this.coinCollision, null, this);
-    game.physics.arcade.collide(this.player, this.worldMap.zombies, this.zombieAndPlayerCollision, null, this);
+    game.physics.arcade.collide(
+        game.player,
+        game.worldMap.coins,
+        game.coinCollision,
+        null, game
+    );
+    game.physics.arcade.collide(
+        game.player,
+        game.worldMap.zombies,
+        game.zombieAndPlayerCollision,
+        null, game
+    );
 
     game.scoreTextValue.text = this.score.toString();
     game.moneyTextValue.text = this.money.toString();
